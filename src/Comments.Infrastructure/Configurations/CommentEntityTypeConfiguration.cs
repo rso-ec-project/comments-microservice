db434805c72347fa041a0dc35854be22fcc73054
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-12-15 11:24:52 +0100

    Create entity configurations & DB context

---
+++ src/Comments.Infrastructure/Configurations/CommentEntityTypeConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Comments.Domain.CommentAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Comments.Infrastructure.Configurations
+{
+    public class CommentEntityTypeConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable("reservation");
+
+            builder.Property(x => x.Content)
+                .HasColumnName("content")
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(x => x.Rating)
+                .HasColumnName("rating")
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAt)
+                .HasColumnName("created_at")
+                .IsRequired();
+
+            builder.Property(x => x.UserId)
+                .HasColumnName("user_id")
+                .IsRequired();
+
+            builder.Property(x => x.ChargingStationId)
+                .HasColumnName("charging_station_id")
+                .IsRequired();
+        }
+    }
+}
